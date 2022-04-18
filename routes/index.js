12b5cfa144dc09b42d7ec7cdf8374c2a74602b6e
hooeni <hooeni@gmail.com>
2022-04-19 00:14:48 +0900

    cookie, 로그인폼 추가

--- routes/index.js
+++ routes/index.js
@@ -16,4 +16,21 @@ router.get('/', function (req, res) {
   res.send(html);
 })
 
+router.get('/login', function (req, res) {
+  var title = 'Login';
+  var description = 'Hello, Node.js';
+  var list = template.list(req.list);
+  var html = template.HTML(title, list,
+    `
+    <form action="login_process" method="post">
+      <p> <input type="text" name="email" placeholder="email"> </p>
+      <p> <input type="password" name="password" placeholder="password"> </p>
+      <p> <input type="submit" > </p>
+    </form>
+    `,
+    `<a href="/topic/create">create</a>`
+  );
+  res.send(html);
+})
+
 module.exports = router;
---
+++ syntax/cookie.js
@@ -0,0 +1,21 @@
+var http = require('http');
+var cookie = require('cookie');
+http.createServer(function (req, res) {
+  console.log(req.headers.cookie);
+  var cookies = {};
+  if (req.headers.cookie !== undefined) {
+    cookies = cookie.parse(req.headers.cookie);
+  }
+
+  console.log(cookies.yummy_cookie);
+  res.writeHead(200, {
+    'Set-Cookie': [
+      'yummy_cookie=choco',
+      'tasty_cookie=strawberry',
+      `permanent=cookies; Max-Age=${60 * 60}`,
+      'Path=Path; Path=/cookie'
+
+    ]
+  });
+  res.end('Cookie!!');
+}).listen(3000);
